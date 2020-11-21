3b1a26ccdaac806951fe2cfdf0f22515fdfded3b
Murilo Moraes <66094955+Moraes97-Murilo@users.noreply.github.com>
2020-11-21 05:40:50 -0300

    Update hackerrank.py
    new challenge

--- hackerrank.py
+++ hackerrank.py
@@ -74,4 +74,13 @@ n = int(input())
 for i in range(1,n+1):
     print (i, end='')
     
+#eighth challenge, list comprehension
+x, y, z, n = (int(input()) for _ in range(4))
+     
+    print([[a,b,c] 
+    for a in range(0,x+1)  
+    for b in range(0,y+1)
+    for c in range(0,z+1) 
+    if a+b+c != n])
+
 #End here    
