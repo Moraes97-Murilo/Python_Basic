2e63acd6d3ced6696e650131e4ae89b49cb00210
Murilo Moraes <66094955+Moraes97-Murilo@users.noreply.github.com>
2020-12-02 08:29:57 -0300

    First graph

--- graphs.py
+++ graphs.py
@@ -1,3 +1,10 @@
 #Lets practice some plots
 
 import matplotlib.pyplot as plt
+
+x = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15] 
+y = [0,0.8,1.7,2.6,3.8,4.5,5.5,6.3,7.4,8.2,7.3,6.4,5.8,4.7,3.4,1.9]
+
+plt.plot(x,y)
+plt.show()
+
