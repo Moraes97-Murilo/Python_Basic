c3eb98f260bf365b90b74e81a392866c5e4a4068
Murilo Moraes <66094955+Moraes97-Murilo@users.noreply.github.com>
2020-12-02 08:45:20 -0300

    second graph

--- graphs.py
+++ graphs.py
@@ -1,5 +1,5 @@
 #Lets practice some plots
-
+#first try
 import matplotlib.pyplot as plt
 
 x = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15] 
@@ -8,3 +8,14 @@ y = [0,0.8,1.7,2.6,3.8,4.5,5.5,6.3,7.4,8.2,7.3,6.4,5.8,4.7,3.4,1.9]
 plt.plot(x,y)
 plt.show()
 
+#second try
+
+a = []
+b = []
+
+for i in range(16):
+  a.append(i)
+  b.append(input("Type the value of Y: "))  
+
+plt.bar(a,b)
+plt.show()
