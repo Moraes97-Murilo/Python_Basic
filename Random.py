eed1277b38b5e019dac8182e2bd0bd4f1d24e179
Murilo Moraes <66094955+Moraes97-Murilo@users.noreply.github.com>
2021-01-01 23:31:39 -0300

    Update Random.py

--- Random.py
+++ Random.py
@@ -1,2 +1,9 @@
 #let's play some random choices with python
 import random
+
+elect = "1s²  2s²  2p6  3s²  3p6  4s²  3d10  4p6  5s²  4d10  5p6  6s²  4f14  5d10"
+list_e = elect.split("  ")
+
+atom = random.choice(list_e)
+
+print("What element from periodic table have final electronic configuration like {} ?".format(atom))
